5dbccc408f6f57b5ff87ac6036f5a75baf58c0c9
Mike <marrone.mike@gmail.com>
2013-11-23 07:35:43 -0500

    more fleshed out Card test spec

--- tests/jasmine/spec/models/CardSpec.js
+++ tests/jasmine/spec/models/CardSpec.js
@@ -1,6 +1,6 @@
 define(["models/card"], function(Card) {
 
-describe("Tests for Card Model", function() {
+return describe("Tests for Card Model", function() {
 
     beforeEach(function() {
 
@@ -15,6 +15,39 @@ describe("Tests for Card Model", function() {
         expect(card.get("face")).toBe(0);
         expect(card.get("matched")).toBe(false);
         expect(card.get("faceUp")).toBe(false);
+    });
+
+    it("has pre-defined face values for the class", function() {
+        expect(Card.FACES).toBeDefined();
+    });
+
+    it("will not match a different face", function() {
+        var card = new Card({face: Card.FACES.TOPHAT});
+        var card2 = new Card({face: Card.FACES.SHELLY})
+        expect(card.matchesFace(card2)).toBe(false);
+    });
+
+    it("will match the same face", function() {
+        var card = new Card({face: Card.FACES.TOPHAT});
+        var card2 = new Card({face: card.get("face")});
+        expect(card.matchesFace(card)).toBe(true);
+        expect(card.matchesFace(card2)).toBe(true);
+    });
+
+    it("reports it is face up when face showing", function() {
+        var card = new Card();
+        card.set("faceUp", true);
+        expect(card.isFaceUp()).toBe(true);
+        expect(card.isFaceDown()).toBe(false);
+    });
+
+    it("reports it is face down when face is not showing", function() {
+        var card = new Card();
+        expect(card.isFaceUp()).toBe(false);
+        expect(card.isFaceDown()).toBe(true);
+        card.set("faceUp", false);
+        expect(card.isFaceUp()).toBe(false);
+        expect(card.isFaceDown()).toBe(true);
     });
 
 });
